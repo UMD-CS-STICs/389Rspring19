9217cfd1162349f746b62e651f686245379dfd82
= <michael.r1797@gmail.com>
2019-03-29 15:28:05 -0400

    Added forensics II hw

---
+++ assignments/6_Forensics_II/stub.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+import sys
+import struct
+
+
+# You can use this method to exit on failure conditions.
+def bork(msg):
+    sys.exit(msg)
+
+
+# Some constants. You shouldn't need to change these.
+MAGIC = 0xdeadbeef
+VERSION = 1
+
+if len(sys.argv) < 2:
+    sys.exit("Usage: python stub.py input_file.fpff")
+
+# Normally we'd parse a stream to save memory, but the FPFF files in this
+# assignment are relatively small.
+with open(sys.argv[1], 'rb') as fpff:
+    data = fpff.read()
+
+# Hint: struct.unpack will be VERY useful.
+# Hint: you might find it easier to use an index/offset variable than
+# hardcoding ranges like 0:8
+magic, version = struct.unpack("<LL", data[0:8])
+
+if magic != MAGIC:
+    bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
+
+if version != VERSION:
+    bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
+
+print("------- HEADER -------")
+print("MAGIC: %s" % hex(magic))
+print("VERSION: %d" % int(version))
+
+# We've parsed the magic and version out for you, but you're responsible for
+# the rest of the header and the actual FPFF body. Good luck!
+
+print("-------  BODY  -------")
