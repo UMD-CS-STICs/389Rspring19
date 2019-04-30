0d3324481eb68431670f26f72d3c0f4e3eb42daf
abloch0 <alexbloch@outlook.com>
2019-04-30 18:42:58 -0400

    ghfrs

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,19 +4,18 @@ import hashlib
 import string
 
 def crack():
-    hashes = open('hashes.txt') # open and read hashes.txt
+    hashes = [l.rstrip() for l in open('hashes.txt')] # open and read hashes.txt
     passwords = open('passwords.txt')  # open and read passwords.txt
     characters = string.ascii_lowercase
     
     for p in passwords:
         for c in characters:
             # crack hashes
-            val = c + p
+            val = c + p.rstrip()
             #print(val)
             h = hashlib.sha256(val.encode('ascii'))
-            po = 'dbec1495345f5a1573a0dd437c207cacc844f74b8a7b030c858f4f660bf9484f'
             #print(h.hexdigest())
-            if h.hexdigest() == po:
+            if h.hexdigest() in hashes:
                 print(val +' : ' + h.hexdigest())
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -2,21 +2,43 @@
 
 import hashlib
 import string
-import socket
+import socket 
 import time
 
+def crack( data, passwords, characters, s):
+    for p in passwords:
+        for c in characters:
+            # crack hashes
+            val = c + p.rstrip()
+            #print(val)
+            h = hashlib.sha256(val.encode('ascii'))
+            #print(h.hexdigest())
+            hd = hashlib.new('sha256', data)
+            #print(hd.hexdigest())
+            if h.hexdigest() == hd.hexdigest():
+                s.send((val +'\n').encode())
+
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    passwords = open('passwords.txt')  # open and read passwords.txt
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
+
     # parse data
     # crack 3 times
 
+    data = s.recv(1024)
+    crack(data, passwords, characters, s)
+    time.sleep(1)
+    data = s.recv(1024)
+    crack(data, passwords, characters, s)
+    time.sleep(1)
+    data = s.recv(1024)
+    crack(data, passwords, characters, s)
+    time.sleep(1)
+    
 if __name__ == "__main__":
     server_crack()
